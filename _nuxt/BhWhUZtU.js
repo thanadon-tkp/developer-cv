2bc936f34ca637e8942e8ff580176b1e086a88cf
Thanadon <thanadon.tdev@gmail.com>
2025-01-05 22:51:05 +0700

    Updates

--- _nuxt/BhWhUZtU.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,x as c,v as n}from"./B55l-d72.js";const o={};function r(t,a){return n(),c("div",null," Home page ")}const _=e(o,[["render",r]]);export{_ as default};
--- _nuxt/C_RJnCUb.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,v as r,x as n,J as o}from"./B55l-d72.js";const s={};function a(e,c){return r(),n("div",null,[o(e.$slots,"default")])}const _=t(s,[["render",a]]);export{_ as default};
